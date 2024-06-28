7453c1b80903ca3acba86990fa3870be272813bf
SYED AMEER JUNAID <junaid0825@gmail.com>
2024-06-28 21:28:08 +0530

    second

---
+++ app/page.tsx
@@ -0,0 +1,27 @@
+import MoviesCarousal from "@/components/MoviesCarousal";
+import CarousalBannerWrapper from "@/components/CarousalBannerWrapper";
+import { getCompanies, getDiscoverTv, getNowPlaying, getPopularMovies, getTopRatedMovies, getTrendingMovie, getTrendingTv, getUpcommingMovies } from "@/lib/getMovies";
+
+export default async function Home() {
+  const upcomingMovies = await getUpcommingMovies();
+  const topRatedMovies = await getTopRatedMovies();
+  const popularMovies = await getPopularMovies();
+  const tv = await getDiscoverTv();
+  const nowPlaying = await getNowPlaying();
+  const trendingMovies = await getTrendingMovie();
+  const trendingTv = await getTrendingTv();
+  return (
+    <main className="relative">
+      <CarousalBannerWrapper id={0} keyWord={""} />
+      <div className="absolute top-[70%] left-0 w-full z-40">
+        <MoviesCarousal movies={upcomingMovies} title="UpComing" />
+        <MoviesCarousal movies={topRatedMovies} title="Top Rated" />
+        <MoviesCarousal movies={popularMovies} title="Popular" />
+        <MoviesCarousal movies={tv} title="TV Shows" />
+        <MoviesCarousal movies={nowPlaying} title="Now Playing" />
+        <MoviesCarousal movies={trendingMovies} title="Trending Movies" />
+        <MoviesCarousal movies={trendingTv} title="Trending TV Shows" />
+      </div>
+    </main>
+  );
+}
